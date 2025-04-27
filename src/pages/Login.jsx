8a98cbf07cf4a88a7acffe6fa95cf184570e38d8
Rodbak123 <rodolph.bako@acity.edu.gh>
2025-04-27 21:38:27 +0000

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,7 +10,13 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('https://campus-hostel-backend-ztbe.onrender.com/api/auth/login', { email, password });
+      
+      const res = await axios.post('https://campus-hostel-backend-ztbe.onrender.com/api/auth/login', {
+        email: form.email,
+        password: form.password
+      });
+
+      
       localStorage.setItem('token', res.data.token);
       setMsg('✅ Login successful!');
       setTimeout(() => navigate('/dashboard'), 1000);
